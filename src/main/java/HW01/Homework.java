3f23ed0bc7a62640c9cc2019b118ea3637ebbd25
IAbeteEtMechante <pierre.machine.learning@gmail.com>
2020-11-10 12:57:03 +0700

    Change the syntax of Homework1.java and Task1.java to correct errors. Add input.txt

--- src/main/java/HW01/Homework.java
+++ src/main/java/HW01/Homework.java
@@ -1,35 +1,42 @@
+package HW01;
+
 import java.util.Scanner;
 import java.io.*;
 
-public class Homework2 {
-    
+public class Homework1 {
     private static int[] readFile() {
         try {
-            Scanner sc = new Scanner(new File("input.txt"));
+            Scanner sc = new Scanner(new File("src/main/java/hw1/input.txt"));
             int a = sc.nextInt();
-            int b = sc.nextInt()
-            return new int[] { a, b };
+            int b = sc.nextInt();
+            return new int[]{a, b};
         } catch (FileNotFoundException ex) {
             return null;
         }
     }
-    
+
     private static void writeFile(int number) {
         try {
             Writer writer = new BufferedWriter(
                     new OutputStreamWriter(
-                        new FileOutputStream("output.txt"), "ascii"));
-            writer.write(new Integer(number).toString();)
+                            new FileOutputStream("src/main/java/hw1/output.txt"), "ascii"));
+            writer.write(Integer.valueOf(number).toString());
             writer.close();
-          } catch (IOException ex) {
-              
-          }
+        } catch (IOException ex) {
+            System.out.println("IO Exception error.");
+        }
     }
 
-    
+
     public static void main(String[] args) {
-        int[] vals = readFile()
-        long mult = vals[0] * vals[1];
-        writeFile(mult);
+        int[] vals = readFile();
+        if (vals == null)
+            System.out.println("You didn't provide an input.txt file, please add an input.txt file to this folder and retry.'");
+        else {
+            long mult = vals[0] * vals[1];
+            writeFile( (int) mult);
+        }
     }
+}
+
 
--- src/main/java/HW01/Task1.java
+++ src/main/java/HW01/Task1.java
@@ -1,12 +1,15 @@
-public class Task_1 {
-        
+package HW01;
+
+public class Task1 {
+
     private static int give_me_one() {
-        return "1";
+        return 1;
     }
-    
+
     public static void Main(String[] args) {
-        int one = give_me_one()
-        int two = one * 2
-        System.out.println(two)
+        int one = give_me_one();
+        int two = one * 2;
+        System.out.println(two);
     }
 }
+
