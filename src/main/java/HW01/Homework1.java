29f492b87accad77b26b5c019c0ab6342002065e
IAbeteEtMechante <pierre.machine.learning@gmail.com>
2020-11-10 12:49:47 +0700

    Create the directory and initialize with gradle init --type java-library

---
+++ src/main/java/HW01/Homework1.java
@@ -0,0 +1,42 @@
+package HW01;
+
+import java.util.Scanner;
+import java.io.*;
+
+public class Homework1 {
+
+    private static int[] readFile() {
+        try {
+            Scanner sc = new Scanner(new File("HW01/input.txt"));
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            return new int[] { a, b };
+        } catch (FileNotFoundException ex) {
+            return null;
+        }
+    }
+
+    private static void writeFile(int number) {
+        try {
+            Writer writer = new BufferedWriter(
+                    new OutputStreamWriter(
+                        new FileOutputStream("HW01/output.txt"), "ascii"));
+            writer.write(new Integer(number).toString());
+            writer.close();
+        } catch (IOException ex) {
+System.out.println("IOException Error.");
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int[] vals = readFile();
+        if (vals == null)
+            System.out.println("You didn't provide an input.txt file, please add an input.txt file to this folder and retry.'");
+        else {
+
+            long mult = vals[0] * vals[1];
+            writeFile( (int) mult);
+        }
+    }
+}
